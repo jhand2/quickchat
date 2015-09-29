835a6c28f0fff7972f8cf9694e36569969cdca95
Jordan Hand <jordanhand@comcast.net>
2015-09-28 20:59:15 -0700

    integrated angular

---
+++ public/js/appRoutes.js
@@ -0,0 +1,14 @@
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', 
+	function($routeProvider, $locationProvider) {
+		// $routeProvider.when('/', {
+		// 	// templateUrl: 'views/home.html',
+		// 	controller: 'MainController'
+		// });
+		// // .when('/artist', {
+		// // 	templateUrl: 'views/artist.html',
+		// // 	controller: "ArtistController"
+		// // });
+
+		// $locationProvider.html5Mode(true);
+	}
+]);
---
+++ public/js/controllers/MainController.js
@@ -0,0 +1,27 @@
+angular.module("MainController", []).controller('MainController', ['$scope',
+	function($scope) {
+		socket = io();
+		$scope.send = function() {
+			var messageContainer = document.getElementById('messages');
+			var messageInput = document.getElementById("message-input");	
+			var message = messageInput.value;
+
+			if (message.length > 0) {
+				messageInput.value = "";
+
+				var newParagraph = document.createElement("P");
+				newParagraph.innerHTML = message;
+				messageContainer.appendChild(newParagraph);
+
+				socket.emit('chat', message);
+			}
+		}
+
+		socket.on('chat', function(msg) {
+			var messageContainer = document.getElementById('messages');
+			var newParagraph = document.createElement("P");
+			newParagraph.innerHTML = msg;
+			messageContainer.appendChild(newParagraph);
+		})
+	}
+]);
